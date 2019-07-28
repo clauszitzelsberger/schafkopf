2fdcf2681c20f9293df1c7944e82c3fbb43f47a7
clauszitzelsberger <40275652+clauszitzelsberger@users.noreply.github.com>
2019-07-28 14:00:55 +0200

    docu

--- helper.py
+++ helper.py
@@ -1,15 +1,27 @@
 # -*- coding: utf-8 -*-
+NoneType = type(None)
 
 class Helper():
+    """Helper functions"""
     
     @staticmethod
     def get_cards(cards, color=None, number=None, trumps=None, state=None):
-        assert type(cards)==list, 'Cards arg must be list type'
-        assert type(color)==list or color is None, 'Color arg must be list type or None'
-        assert type(number)==list or number is None, 'Number arg must be list type or None'
-        assert type(trumps)==bool or trumps is None, 'Trumps arg must be bool type or None'
+        """Returns subset of cards based on given criteria
+        
+        params
+        =====
+            cards (list), remaining cards
+            color (list), list of colors to be selected from cards list
+            number (list), list of numbers to be selected from cards list
+            trumps (bool), whether trumps should be selected of not
+            state (state class), to determine which trumps to be selected based on game
+        """
+        assert isinstance(cards, list)
+        assert isinstance(color, (list, NoneType))
+        assert isinstance(number, (list, NoneType))
+        assert isinstance(trumps, (bool, NoneType))
         if type(trumps)==bool:
-            assert state is not None, 'State needs to be defined'
+            assert state is not None, 'State needs to be defined if trumps is criteria'
             
         card_colors, card_numbers = Helper.__card_lists(cards)
         
@@ -31,6 +43,7 @@ class Helper():
         
     @staticmethod        
     def get_trumps(cards, state):
+        """Returns subset of trumps in cards list based on state (selected game)"""
         
         card_colors, card_numbers = Helper.__card_lists(cards)
         
@@ -49,15 +62,16 @@ class Helper():
     
     @staticmethod    
     def __card_lists(cards):
+        """Returns two lists, one for color, one for number given a cards list"""
         return [card.color for card in cards], [card.number for card in cards]
     
     @staticmethod
     def __intersection(lst1, lst2): 
+        """Returns all elements which are in both lists"""
         return [i for i in lst1 if i in lst2] 
     
     @staticmethod
     def __difference(lst1, lst2):
-        """
-        set theoretic difference of lst1 and lst2 (lst1 \ lst2)"""
+        """Returns theoretic difference of lst1 and lst2 (lst1 \ lst2)"""
         return [i for i in lst1 if i not in lst2]
         
