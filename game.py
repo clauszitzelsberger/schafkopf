13161b5bf0e48ae9bff6b64af0963141c281e36f
clauszitzelsberger <40275652+clauszitzelsberger@users.noreply.github.com>
2019-07-28 15:25:55 +0200

    refactor: change to dict description of game rather than object

--- game.py
+++ game.py
@@ -12,19 +12,12 @@ sauspiele = games[:3]
 soli = games[3:7]
 wenz = games[7]
 
-class Game():
-    def __init__(self, color, kind):
-        
-        if [color, kind] != [None, None]:            
-            assert type(color)==str or color is None, 'color argument must be string or None type'
-            assert type(kind)==str, 'kind argument must be string type'
-            assert [color, kind] in games, 'Invalid game defined: ' + color + ' ' + kind
-            
-            self.kind = kind
-            self.color = color
-            
-            self.reward = rewards[self.kind]
-
-
-if __name__ == '__main__':
-    game = Game(None, 'wenz')
+def initialize(color, kind):
+    assert type(color)==str or color is None, 'color argument must be string or None type'
+    assert type(kind)==str, 'kind argument must be string type'
+    assert [color, kind] in games, 'Invalid game defined: ' + color + ' ' + kind
+    
+    game = {'kind': kind,
+            'color': color,
+            'reward': rewards[kind]}
+    return game
